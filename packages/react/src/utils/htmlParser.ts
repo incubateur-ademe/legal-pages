a9bbc6c1b4489d8765c45499be62607780fe95bb
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2025-11-12 16:40:25 +0100

    chore: adapt ci

--- packages/react/src/utils/htmlParser.ts
+++ packages/react/src/utils/htmlParser.ts
@@ -14,7 +14,7 @@ interface HtmlPropsMapping {
   PrivacyPolicy: PrivacyPolicyProps;
 }
 
-// @eslint-disable import/no-unresolved
+/* @eslint-disable import/no-unresolved */
 const htmlImporter = {
   LegalNotice: (withBeta: boolean) =>
     withBeta
@@ -36,4 +36,4 @@ export const htmlParser = async <T extends keyof HtmlPropsMapping>(template: T, 
     [template, data],
   );
 };
-// @eslint-enable import/no-unresolved
+/* @eslint-enable import/no-unresolved */
